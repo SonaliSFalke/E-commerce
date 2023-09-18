e35d42693a73c3f52b7e7d5b3436359a0b7f2dd0
SonaliSFalke <137392667+SonaliSFalke@users.noreply.github.com>
2023-09-18 14:22:30 +0530

    upload all files

---
+++ script.js
@@ -0,0 +1,10 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
